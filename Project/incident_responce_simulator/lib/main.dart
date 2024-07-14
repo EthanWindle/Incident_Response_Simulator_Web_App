6ee7658288de8cfae3a08cc4d58c8ab825fef8a8
windleetha <windleetha@ecs.vuw.ac.nz>
2024-07-15 11:57:53 +1200

    added material button for each section

--- Project/incident_responce_simulator/lib/main.dart
+++ Project/incident_responce_simulator/lib/main.dart
@@ -14,10 +14,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Incident Response',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 1, 21, 151), 
-        primary: Color.fromARGB(255, 1, 21, 151),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 1, 21, 151), 
+        primary: const Color.fromARGB(255, 1, 21, 151),
         surface: Colors.white),
-        textTheme: TextTheme(
+        textTheme: const TextTheme(
           displayLarge: TextStyle(
           color: Colors.white,
           ),
@@ -39,23 +39,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _selectedScenario = "not selected";
-
-  void _selecteScenario(String str) {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _selectedScenario = str;
-    });
-  }
-
   void _comfirm(){
      Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => choicePage()),
+              MaterialPageRoute(builder: (context) => const choicePage()),
             );
   }
 
@@ -70,7 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title,
-        style: TextStyle(color: Colors.white)),
+        style: const TextStyle(color: Colors.white)),
       ),
       body: Center(
         child: Column(
@@ -79,24 +66,47 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              _selectedScenario,
-              style: Theme.of(context).textTheme.headlineMedium,
+            MaterialButton(
+              onPressed: () => _comfirm(),
+              color: Theme.of(context).primaryColor,
+              height: 140.0,
+              minWidth: 100.0,
+              child: const Text(
+                'Run by Yourself',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ), 
             ),
+            MaterialButton(
+              onPressed: () => _comfirm(),
+              color: Theme.of(context).primaryColor,
+              height: 140.0,
+              minWidth: 100.0,
+              child: const Text(
+                'Host',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ), 
+            ),
+            MaterialButton(
+              onPressed: () => _comfirm(),
+              color: Theme.of(context).primaryColor,
+              height: 140.0,
+              minWidth: 100.0,
+              child: const Text(
+                'Join',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ), 
+            )
           ],
         ),
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        /*onPressed: () => _selecteScenario("111"),
-        tooltip: 'set selection',
-        child: const Icon(Icons.add),
-      ),
-
-      comfirmButton: FloatingActionButton(*/
-        onPressed: () => _comfirm(),
-        tooltip: 'set selection',
-        child: const Icon(Icons.add),
       ),
     );
   }
