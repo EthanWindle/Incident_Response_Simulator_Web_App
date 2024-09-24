bed3ee9c46e8774a02483bc61ba4e801bb8ddc84
windleetha <windleetha@ecs.vuw.ac.nz>
2024-09-24 14:20:23 +1200

    update page to work with firebase

--- Project/incident_responce_simulator/.firebase/incident-response-simulator/hosting/flutter_service_worker.js
+++ Project/incident_responce_simulator/.firebase/incident-response-simulator/hosting/flutter_service_worker.js
@@ -24,14 +24,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "de9b4ea3c6a5404f9357b1027434ee25
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "2752a9f37aa319fb27a5f8dd3e91d293",
+"flutter_bootstrap.js": "c54f7f5b16cf6e747bdadc5279d11630",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "0ebc4b71314242353afd782c867cbbc8",
 "/": "0ebc4b71314242353afd782c867cbbc8",
-"main.dart.js": "cfa37916ce1baab7c0c98cac82aeec1d",
+"main.dart.js": "473ef5ffd78e12f9e32e7ef580892998",
 "manifest.json": "71064e9041d0a8ba1ac08ad3b5cf6f91",
 "version.json": "4dbed5cb23451f6797322fa6146e5044"};
 // The application shell files that are downloaded before a service worker can
